0ce98fe71076f4f5b6a2a3000ec0cf27aa808858
Antonio Díaz <20223TN047@utez.edu.mx>
2024-05-03 21:32:29 -0600

    Refactor abstract class Controller in Controller.php Add routes for HomeController and PostController in web.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+abstract class Controller //abstract = class no puede ser instanciada
 {
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,52 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
+//Clases home
+Route::get(
+    '/',
+    HomeController::class
+
+);
+
+//Clases post
+Route::get(
+    '/posts',
+    [
+        PostController::class, 'index'
+    ]
+);
+
+Route::get(
+    '/posts/create',
+    [
+        PostController::class, 'create'
+    ]
+);;
+
+Route::get(
+    '/posts/{post}',
+    [
+        PostController::class, 'show'
+    ]
+);
+
+
+
+/*
+Route::get('/posts/{post}/{category?}', function ($post, $category = null) {
+    if ($category) {
+        return "Postagem: " . $post . " Categoria: " . $category;
+    } else {
+        return "Postagem: " . $post;
+    }
 });
+*/
+
+//Get 
+//Post
+//Put
+//Delete
+//Patch
